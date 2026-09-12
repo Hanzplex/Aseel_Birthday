c6bafa19550da277bff0d48cb39c05cd413af92e
Hanzplex <165448435+Hanzplex@users.noreply.github.com>
2026-09-12 21:55:10 +0300

    Update music options in settings.js

--- jscp/settings.js
+++ jscp/settings.js
@@ -33,7 +33,8 @@ window.lastIsSaveState = false;
 
 const musicOptions = [
     { value: './music/aseel.mp3', label: 'Happy Birthday Aseel' },
-    { value: './music/spiderAseel.mp3', label: 'Custom Music (Add your own)' }
+    { value: './music/spiderAseel.mp3', label: 'Spider Aseel' },
+    { value: './music/custom.mp3', label: 'Custom Music (Add your own)' }
 ];
 const gifOptions = [
     { value: '', label: 'None' }
